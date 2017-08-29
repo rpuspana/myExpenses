02ee54f4cf3ae1be3c9acff8f558c757913c9d9a
rpuspana <rpuspana@gmail.com>
2017-08-29 19:25:08 +0300

    Add obj with pblGetInput() to uiController .Get user input value from the UI in ctrlAddItem.Register keypress event in the Value enter field.Add pbl and prv to var names

--- js/app.js
+++ js/app.js
@@ -16,7 +16,23 @@ var budgetController = (function() {
 // UI module manipulates the UI of the app
 var uiController = (function() {
 
-    // some code
+    return {
+        // public method to retrieve and return all three field values as strings from the UI
+        pblGetInput: function() {
+
+            // return an object with all three fields values as strings from the UI
+            return {
+                // return "inc" or "exp" from the transaction type list
+                type: document.querySelector(".add__type").value,
+
+                // return the transaction description as string
+                description: document.querySelector(".add__description").value,
+
+                // return the transaction value as a string
+                value: document.querySelector(".add__value").value
+            };
+        }
+    }
 
 })();
 
@@ -24,8 +40,14 @@ var uiController = (function() {
 // App controller for communicating between the budgetController and the UIconroller
 var controller = (function(budgetCtrl, UIctrl) {
 
-    var ctrlAddItem = function() {
+    // private method for :
+    // - retrieving all three user input field values
+    var prvCtrlAddItem = function() {
+
         // get the field input data(expense or income)
+        var userInput = uiController.pblGetInput();
+        console.log(userInput);
+
         // add the item to the budget controller
         // add the new item to the UI
         // update the budget taking into account the expense/budget just entered
@@ -34,14 +56,14 @@ var controller = (function(budgetCtrl, UIctrl) {
     }
 
     // register click event for the button with the tick sign
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem, false);
+    document.querySelector(".add__btn").addEventListener("click", prvCtrlAddItem, false);
 
     // register Enter keypress event for the global object. Used only for the ENTER key
-    document.addEventListener("keypress", function(event) {
+    document.getElementById("enterSum").addEventListener("keypress", function(event) {
 
         // use event.which to assure comptatibility with IE 9..11, Edge 14, UC Browser for Android 11.4
         // code 13 is returned when the "ENTER" key is pressed
-        if (event.key === "Enter" || event.which === 13) { ctrlAddItem(); }
+        if (event.key === "Enter" || event.which === 13) { prvCtrlAddItem(); }
     }, false);
 
 })(budgetController, uiController);
