1cebf1bf3cd75e2ba67062078dd178594f41d7e1
rpuspana <rpuspana@gmail.com>
2017-09-14 13:28:17 +0300

    Edit the roundDecimal() comment correcting mistakes

--- js/library.js
+++ js/library.js
@@ -8,9 +8,9 @@
 // Round the n-th decimal of a float number
 // number        Number  the floating point number to be rounded
 // decimalIndex  Number  the decimal position to be rounded
-//   E.g  decimalIndex = 1, the first digit shuld be rounded
-//   E.g  decimalIndex = 2, the second digit shuld be rounded
-// return        Number  the number with a rounded decimal
+//   E.g  decimalIndex = 1, the first decimal will be rounded
+//   E.g  decimalIndex = 2, the second decimal will be rounded
+// return        Number  a floating point number with the required decimal rounded
 function roundDecimal(number, decimalIndex) {
     var multiplier = Math.pow(10, decimalIndex || 0);
     return Math.round(number * multiplier) / multiplier;
